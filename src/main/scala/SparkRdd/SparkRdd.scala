14d4d03880f860d074470417372ab17f552914e3
Muhammad Saeed <saeedbutt320@.com>
2019-03-30 12:06:07 +0100

    With light performance tuning

--- src/main/scala/SparkRdd/SparkRdd.scala
+++ src/main/scala/SparkRdd/SparkRdd.scala
@@ -1,6 +1,8 @@
 package SparkRdd
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.sql
+
 
 object SparkRdd {
   def main(args: Array[String]): Unit = {
@@ -9,8 +11,12 @@ object SparkRdd {
     var conf = new SparkConf().setAppName("SparkRdd").setMaster("local[*]");
     var sc = new SparkContext(conf);
     sc.setLogLevel("ERROR");
-    val data = sc.textFile("file:///E:/Projects/Big Data/Big Datasets/ml-20m/movies.csv");
-    data.foreach(println);
+    // we have use 4 core processor. 
+    var movies = sc.textFile("file:///E:/Projects/Big Data/Big Datasets/ml-20m/movies.csv",4);
+    var ratings = sc.textFile("file:///E:/Projects/Big Data/Big Datasets/ml-20m/ratings.csv");
+    var uniondata  = movies.union(ratings);
+    
+    ratings.foreach(println);
     
     
   }
